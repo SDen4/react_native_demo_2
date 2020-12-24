ad56ea28f883a1f18e524177d7efbf1bef4c8ab8
SDen4 <asw325@yandex.ru>
2020-12-24 14:39:39 +0500

    navigation plugin

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
-import { Text, View } from "react-native";
-import { AppLoading } from "expo";
-import { bootstrap } from "./src/bootstrap";
+import React, { useState } from 'react';
+// import { Text, View } from "react-native";
+import { AppNavigation } from './src/navigation/AppNavigation';
+// import { AppLoading } from "expo";
+import AppLoading from 'expo-app-loading';
+import { bootstrap } from './src/bootstrap';
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
 
     if (!isReady) {
-        return <AppLoading startAsync={bootstrap} onFinish={() => setIsReady(true)} onError={(err) => console.log(err)} />;
+        return (
+            <AppLoading
+                startAsync={bootstrap}
+                onFinish={() => setIsReady(true)}
+                onError={(err) => console.log(err)}
+            />
+        );
     }
 
-    return (
-        <View>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <AppNavigation />;
 }

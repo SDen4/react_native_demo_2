f8b7a2f7a991ae7284cb22e11ba7972742cc0bc6
SDen4 <asw325@yandex.ru>
2020-12-25 15:21:47 +0500

    delete post modal window

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Image, Button, Alert } from 'react-native';
+import { THEME } from '../theme';
+import { DATA } from '../data';
 
 export const PostScreen = ({ navigation }) => {
     const postId = navigation.getParam('postId');
+
+    const post = DATA.find((data) => data.id === postId);
+
+    const removeHandler = () => {
+        Alert.alert(
+            'Delete the post',
+            'Are you sure ?..',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                { text: 'Delete', style: 'destructive', onPress: () => {} }
+            ],
+            { cancelable: false }
+        );
+    };
+
     return (
-        <View style={styles.container}>
-            <Text>{postId}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{ uri: post.img }} style={styles.image}></Image>
+            <View style={styles.textWrapper}>
+                <Text style={styles.text}>{post.text}</Text>
+            </View>
+            <Button title="Delete" color={THEME.DANGER_COLOR} onPress={removeHandler} />
+        </ScrollView>
     );
 };
 
 PostScreen.navigationOptions = ({ navigation }) => {
     const date = navigation.getParam('date');
     return {
-        headerTitle: 'Post from' + new Date(date).toLocaleDateString()
+        headerTitle: 'Post from' + new Date(date).toLocaleDateString(),
     };
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    textWrapper: {
+        padding: 10,
+    },
+    text: {
+        fontFamily: 'open-regular',
     },
 });
